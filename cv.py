a8c287576fa15104b0fe9d5e683e9c5e17179a4a
Ben Pastel <benpastel@gmail.com>
2016-06-29 16:39:47 -0700

    cv

---
+++ cv.py
@@ -0,0 +1,44 @@
+import numpy as np
+import pandas as pd
+from models import *
+
+train_file = "data/slim_train.csv"
+train_cols = (
+	"week",
+	"client_id",
+	"product_id",
+	"net_units_sold"
+)
+train_dtypes = {
+	"week": np.int8,
+	"client_id": np.int32,
+	"product_id": np.int32,
+	"net_units_sold": np.int32
+}
+train_weeks = range(3, 9)
+dev_weeks = range(9, 10)
+
+def RMSLE(preds, actuals):
+	diffs = np.log(preds + 1) - np.log(actuals + 1)
+	return np.sqrt( np.average(diffs ** 2) )
+
+print "loading training file..."
+all_train = pd.read_csv(train_file, names=train_cols, dtype=train_dtypes, engine='c')
+print "loaded %d samples" % len(all_train)
+
+print "adding (client, product) pair keys"
+# the product_ids are all less than 50k
+all_train["key"] = all_train["client_id"].astype(np.int64) * 50000 + all_train["product_id"]
+
+print "splitting into train / dev..."
+train = all_train[all_train['week'].isin(train_weeks)]
+dev = all_train[all_train['week'].isin(dev_weeks)]
+print "%d in train, %d in dev, %d total" % (len(train), len(dev), len(all_train))
+
+print "making predictions..."
+preds = simple_avg(train, dev)
+
+print "calculating RMSLE..."
+rmsle = RMSLE(preds, dev["net_units_sold"])
+print "RMSLE: ", rmsle
+
